87864504601efd202e79de1ca1b379c48f6acbf2
Chikoom <callforcopy@gmail.com>
2020-04-16 01:41:02 +0300

    created the basic app - some bug need fixing

--- statics/script.js
+++ statics/script.js
@@ -1,7 +1,32 @@
 const randomWordURL = "https://random-word-api.herokuapp.com/word?number=1";
-const giphyURL = "api.giphy.com/v1/gifs/random?api_key=DKiA3kL5riYRm73jOgdluRNehaCQlaex&tag=mama";
+const giphyURL = "https://api.giphy.com/v1/gifs/random?api_key=DKiA3kL5riYRm73jOgdluRNehaCQlaex&tag=";
+var startWord="Hello World";
 
 
-function setup() {
+function setup(){
+	fetch(randomWordURL)
+	.then(response => response.json())
+	.then(json => {
+		startWord = json[0].toUpperCase();
+		let title = document.getElementsByClassName('title')[0];
+		title.innerHTML = startWord;
+		return fetchGiphyImage(startWord);
+	})
+	.then(response => response.json())
+	.then(json => {
+		let bgimgURL = json.data.images['fixed_height'].url;
+		let bgimg = document.getElementsByClassName('bgimg')[0];
+		bgimg.style.backgroundImage = `url('${bgimgURL}')`;
+	})
+	.catch(err => {
+		console.log(err)
+		setup();
+	});
+}
 
+setup();
+
+
+function fetchGiphyImage(rendomWord){
+	return fetch(giphyURL + rendomWord);
 }
