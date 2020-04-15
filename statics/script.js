99e26d27b1dc3528a4d30f7a4c00457ee62aff18
Chikoom <callforcopy@gmail.com>
2020-04-16 00:54:18 +0300

    added Giphy and Random-Word URLs

---
+++ statics/script.js
@@ -0,0 +1,7 @@
+const randomWordURL = "https://random-word-api.herokuapp.com/word?number=1";
+const giphyURL = "api.giphy.com/v1/gifs/random?api_key=DKiA3kL5riYRm73jOgdluRNehaCQlaex&tag=mama";
+
+
+function setup() {
+
+}
