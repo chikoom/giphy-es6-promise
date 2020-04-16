3ef7f2c9beb0e0d43dcad38eaafb785c5effc110
Chikoom <callforcopy@gmail.com>
2020-04-16 10:40:00 +0300

    fixed issue with giphy returns black search result

--- statics/script.js
+++ statics/script.js
@@ -1,6 +1,6 @@
 const randomWordURL = "https://random-word-api.herokuapp.com/word?number=1";
 const giphyURL = "https://api.giphy.com/v1/gifs/random?api_key=DKiA3kL5riYRm73jOgdluRNehaCQlaex&tag=";
-var startWord="Hello World";
+var startWord="Loading...";
 
 
 function setup(){
@@ -8,15 +8,17 @@ function setup(){
 	.then(response => response.json())
 	.then(json => {
 		startWord = json[0].toUpperCase();
-		let title = document.getElementsByClassName('title')[0];
-		title.innerHTML = startWord;
+		
 		return fetchGiphyImage(startWord);
 	})
 	.then(response => response.json())
 	.then(json => {
+		
 		let bgimgURL = json.data.images['fixed_height'].url;
 		let bgimg = document.getElementsByClassName('bgimg')[0];
 		bgimg.style.backgroundImage = `url('${bgimgURL}')`;
+		let title = document.getElementsByClassName('title')[0];
+		title.innerHTML = startWord;
 	})
 	.catch(err => {
 		console.log(err)
